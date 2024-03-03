c0245d4f226e4d23c1b0dc35840f2daba913b1c6
TechieSagar <sk19966313@gmail.com>
2024-03-03 11:31:47 +0530

    game design updated

--- casinoGame.c
+++ casinoGame.c
@@ -4,7 +4,19 @@
 
 int main(){
 
-    
+    printf("\t\tWelcom to Casino Game\t\t");
+    printf("\n");
+    printf("---------------------------------------------------------\n");
+    printf("|\t\t\t\t\t\t\t|\n");
+    printf("|\tA Don \t\t\t\t\t\t|\n");
+    printf("|\tB Amar Akbar Anthony \t\t\t\t|\n");
+    printf("|\tC Sahib Biwi or Ghulam \t\t\t\t|\n");
+    printf("|\tD Dharam Veer \t\t\t\t\t|\n");
+    printf("|\tE Kiss Kiss Ko Pyar Karoon \t\t\t|\n");
+    printf("|\tF Ghulam \t\t\t\t\t|\n");
+    printf("|\t\t\t\t\t\t\t|");
+    printf("\n---------------------------------------------------------\n");
+
 
     return 0;
 }
