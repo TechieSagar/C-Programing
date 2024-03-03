fe7b2216bd8ad3d410f2f237415192f2c50a5547
TechieSagar <sk19966313@gmail.com>
2024-03-03 14:31:56 +0530

    switch case added

--- casinoGame.c
+++ casinoGame.c
@@ -1,8 +1,14 @@
 // 1st Project - Casino Game in C programming
 
 #include <stdio.h>
+#include <string.h>
+
+void getInput();
+void getGame(char);
 
 int main(){
+
+    char input;
 
     printf("\t\tWelcom to Casino Game\t\t");
     printf("\n");
@@ -16,7 +22,53 @@ int main(){
     printf("|\tF Ghulam \t\t\t\t\t|\n");
     printf("|\t\t\t\t\t\t\t|");
     printf("\n---------------------------------------------------------\n");
+    printf("\n\n");
 
+    printf("Choose Any game from the above: "); // user need to choose alphabet like a b c etc..
+    scanf("%c", &input);
+    input = tolower(input);
+    getGame(input);
 
     return 0;
+}
+
+void getGame(char input){
+
+    switch(input){
+        case 'a':
+            printf("\t\tDon\t\t\n");
+            break;
+
+        case 'b':
+            printf("\t\tAmar Akbar Anthony\t\t\n");
+            break;
+
+        case 'c':
+            printf("\t\tSahib Biwi or Ghulam\t\t\n");
+            break;
+
+        case 'd':
+            printf("\t\tDharam Veer\t\t\n");
+            break;
+
+        case 'e':
+            printf("\t\tKiss Kiss Ko Pyar Karoon\t\t\n");
+            break;
+
+        case 'f':
+            printf("\t\tGhulam\t\t\n");
+            break;
+
+        default:
+            printf("Wrong input! Try again");
+            break;
+    }
+}
+
+void getInput(){
+    char input;
+    printf("Wrong input! Try again");
+    printf("\nChoose correct game: ");
+    scanf("%c", &input);
+    getGame(input);
 }
